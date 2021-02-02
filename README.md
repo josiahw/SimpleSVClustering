895acb5134c2698062db0487acd8028be8fdf841
Josiah Walker <merlz42@gmail.com>
2021-02-02 11:14:54 +1100

    new stuff

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ A simple implementation of support vector clustering in only python/numpy
 This implements a version of support vector clustering from the paper:
 "A Support Vector Method for Clustering", A. Ben-Hur et al.
 
-The implementation is fairly basic and your mileage may vary, but it seems to work. I am only aware of a few other implementations of support vector clustering, none of which are simplified to this extent. Hopefully somebody finds it useful, or at least informative.
+The implementation is fairly basic but performant for datasets of up to 5,000 (~ 5 min). I am only aware of a few other implementations of support vector clustering, none of which are as approachable. Hopefully somebody finds it useful, or at least informative.
--- SimpleSVC.py
+++ SimpleSVC.py
@@ -4,23 +4,90 @@ Created on Mon Jul 11 20:40:56 2016
 
 @author: josiahw
 """
-import numpy, time, numpy.linalg
+import numpy, time, numpy.linalg, os
+
+# these are not essential, they are specifically for earth movers distance
 from scipy.spatial.distance import cdist
 from scipy.optimize import linear_sum_assignment
-from scipy.interpolate import interp1d
+from scipy import stats
+from multiprocessing import Pool, cpu_count
+from pyemd import emd
+_svc_pts = None
+from KernelMatrix.SymmetricKernelMatrix import SymmetricMatrix
+
+def _kernel_one_to_global(x, kernel, kwargs, idx):
+    xi = numpy.repeat(x.reshape((1,-1)), len(_svc_pts), axis=0)
+    return idx, kernel(xi, _svc_pts, **kwargs)
+
+def _load_global_pts(pts):
+    global _svc_pts
+    _svc_pts = pts
+
+def _kernel_matrix(pts1, pts2, kernel, result_dtype, thread_pool, kwargs):
+    Q = numpy.zeros((len(pts1),len(pts2)), result_dtype)
+    if thread_pool is not None:
+        for idx, result in thread_pool.starmap(_kernel_one_to_many, [(pts1[i], pts2, kwargs, i) for i in range(len(pts1))]):
+            Q[idx] = result
+    else:
+        for i in range(len(pts1)):
+            Q[i] = _kernel_one_to_many(pts1[i], pts2, kernel, kwargs, i)[1]
+    return Q
+
+def _kernel_matrix_global(pts1, kernel, result_dtype, thread_pool, kwargs):
+    """
+    This requires a thread pool loaded with an init function to global pts
+    """
+    global _svc_pts
+    Q = numpy.zeros((len(pts1),len(_svc_pts)), result_dtype)
+    if thread_pool is not None:
+        for idx, result in thread_pool.starmap(_kernel_one_to_global, [(pts1[i], kernel, kwargs, i) for i in range(len(pts1))]):
+            Q[idx] = result
+    else:
+        for i in range(len(pts1)):
+            Q[i] = _kernel_one_to_many(pts1[i], kernel, kwargs, i)[1]
+    return Q
+
+def _sumreduce_global(x, mul_val, kernel, kwargs, idx):
+    return idx, numpy.sum(mul_val * _kernel_one_to_global(x, kernel, kwargs, idx)[1])
+
+def _matrix_sumreduce_global(pts1, mul_val, kernel, result_dtype, thread_pool, kwargs):
+    """
+    multiply matrix by mul_val and sum to reduce
+    """
+    global _svc_pts
+    Q = numpy.zeros((len(pts1),), result_dtype)
+    if thread_pool is not None:
+        for idx, result in thread_pool.starmap(_sumreduce_global, [(pts1[i], mul_val, kernel, kwargs, i) for i in range(len(pts1))]):
+            Q[idx] = result
+    else:
+        for i in range(len(pts1)):
+            Q[i] = _sumreduce_global(pts1[i], mul_val, kernel, kwargs, i)[1]
+    return Q
+
+_dist_mat = None
+def emdRbfKernel2(p1, p2, dim, gamma=1.0):
+    p1 = p1.ravel()
+    p2= p2.ravel()
+    global _dist_mat
+    if _dist_mat is None:
+        as_pt = lambda x: numpy.array([int(x/float(dim[0])), x%dim[0]], dtype=p1.dtype)
+        print(as_pt(78) - as_pt(147))
+        _dist_mat = numpy.array([[numpy.linalg.norm(as_pt(i) - as_pt(j)) for j in range(len(p2))] for i in range(len(p1))], dtype=p1.dtype)
+    print(emd(p1, p2, _dist_mat))
+    return emd(p1, p2, _dist_mat)
 
 def _emd(a,b):
     d = cdist(a, b)
     assignment = linear_sum_assignment(d)
     return d[assignment].sum() / len(a)
 
-def emdKernel(a,b, dim = 1):
+def emdRbfKernel(a,b, dim = (-1,), gamma = 1.0):
     """
     N-dimensional earth-movers distance kernel
     """
-    if len(a.shape) == dim+1:
-        return numpy.array([_emd(a[i,:], b[i,:]) for i in range(a.shape[0])])
-    return _emd(a,b)
+    if len(a.shape) == 2:
+        return numpy.exp(-gamma * numpy.array([_emd(a[i].reshape(dim), b[i].reshape(dim)) for i in range(a.shape[0])]))
+    return numpy.exp(-gamma * _emd(a.reshape(dim),b.reshape(dim)))
 
 def polyKernel(a,b,pwr):
     return numpy.dot(a,b.T)**pwr
@@ -41,12 +108,16 @@ class SimpleSVClustering:
     sv = None
     kernel = None
     tolerance = None
-    verbose = False
+    thread_pool = Pool(6)
+    verbose = True
+    dims = 1
+    class_check_steps = 3
 
     def __init__(self,
                  C,
                  tolerance = 0.001,
                  kernel = numpy.dot,
+                 dtype = numpy.float16,
                  **kwargs
                  ):
         """
@@ -60,50 +131,60 @@ class SimpleSVClustering:
         self.kernel = kernel
         self.tolerance = tolerance
         self.kwargs = kwargs
-        self.dims = 1
-        if "dims" in self.kwargs:
-            self.dims = dims
+        self.dtype =dtype
+        step_size = 1.0/(self.class_check_steps+1)
+        steps = numpy.arange(step_size,1.0-step_size,step_size, dtype=dtype)
+        self.a_weights = steps.reshape(-1,1)
+        self.b_weights = (1-steps).reshape(-1,1)
 
-    def _checkClass(self, a, b, a_pred = None, b_pred = None, n_checks = 4):
+    def _checkClass(self, a, b, steps):
         """
         This does a straight line interpolation between a and b, using n_checks number of segments.
         It returns True if a and b are connected by a high probability region, false otherwise.
         NOTE: authors originally suggested 20 segments but that is SLOOOOOW, so we use 4. In practice it is pretty good.
         """
-        step_size = 1.0/(n_checks+2)
-        for i in numpy.arange(step_size,1.0-step_size,step_size):
-            if self._predict_density(i*a + (1-i)*b) > self.b:
-                return False
-        return True
+        checkVals = self._predict_density(steps[0] * a + (1-steps[0]) * b)
+        for s in steps[1:]:
+            checkVals = numpy.maximum(checkVals, self._predict_density(s * a + (1-s) * b))
+        return checkVals
 
     def _getAllClasses(self, X):
         """
         Assign class labels to each vector based on connected graph components.
         TODO: The outputs of this should really be saved in order to embed new points into the clusters.
         """
-
         #1: build the connected clusters
-        unvisited = list(range(len(X)))
+        unvisited = numpy.array(list(range(len(X))))
         clusters = []
+        step_size = 1.0/(self.class_check_steps+1)
+        steps = numpy.arange(step_size,1.0-step_size,step_size, dtype=X.dtype)
         while len(unvisited):
             #create a new cluster with the first unvisited node
             c = [unvisited[0]]
-            unvisited.pop(0)
+            unvisited = unvisited[1:]
             i = 0
+            t0 = time.time()
             while i < len(c) and len(unvisited):
                 #for all nodes in the cluster, add all connected unvisited nodes and remove them fromt he unvisited list
-                unvisitedNew = []
-                for j in unvisited:
-                    (c if self._checkClass(X[c[i],:],X[j,:]) else unvisitedNew).append(j)
-                unvisited = unvisitedNew
+                candidate_list = X[unvisited]
+                checkVals = self._checkClass(X[c[i]], candidate_list, steps)
+                in_cluster = numpy.where((checkVals <= self.b).ravel())[0]
+                if len(in_cluster) > 0:
+                    c.extend([unvisited[j] for j in in_cluster])
+                    out_cluster = numpy.where((checkVals > self.b).ravel())[0]
+                    unvisited = [unvisited[j] for j in out_cluster]
                 i += 1
             clusters.append(c)
+            if self.verbose:
+                print(f"Clustered {len(X)-len(unvisited)}/{len(X)} in {time.time()-t0}")
 
         #3: group components by classification
-        self.classifications = numpy.zeros(len(X))-1
+        self.classifications = numpy.zeros(len(X))
         for i in range(len(clusters)):
             for c in clusters[i]:
                 self.classifications[c] = i
+        print(f"Clusters: {len(numpy.unique(self.classifications))}")
+        print(f"Cluster sizes: {[len(c) for c in clusters]}")
         return self.classifications
 
     def fit(self, X):
@@ -114,13 +195,7 @@ class SimpleSVClustering:
         """
         Construct the Q matrix for solving
         """
-        self._data = X # TODO: do we need
-        Q = numpy.zeros((len(X),len(X)))
-        for i in range(len(X)):
-            for j in range(i,len(X)):
-                Qval = 1.
-                Qval *= self.kernel(*((X[i,:], X[j,:])), **self.kwargs)
-                Q[i,j] = Q[j,i] = Qval
+        Q = SymmetricMatrix(X, self.kernel, self.kwargs, self.thread_pool, numpy.float16, self.verbose)
 
         """
         Solve for a and w simultaneously by coordinate descent.
@@ -130,37 +205,49 @@ class SimpleSVClustering:
         self.w = numpy.zeros(X.shape[1])
         self.a = numpy.zeros(X.shape[0])
         delta = 10000000000.0
+        maxDelta = delta
+        X_range = range(len(X))[:]
         while delta > self.tolerance:
             delta = 0.
-            for i in range(len(X)):
-                g = numpy.dot(Q[i,:], self.a) - Q[i,i]
-                adelta = self.a[i] - min(max(self.a[i] - g/Q[i,i], 0.0), self.C)
-                self.w += adelta * X[i,:]
+            for i in X_range:
+                g = numpy.dot(Q[i], self.a) / Q[i,i] - 1.0
+                adelta = self.a[i] - min(max(self.a[i] - g, 0.0), self.C)
+                self.w += adelta * X[i]
                 delta += abs(adelta)
                 self.a[i] -= adelta
-            if self.verbose:
-                print ("Descent step magnitude:", delta)
+            delta /=  len(X_range)
+            if delta < maxDelta/2 and maxDelta < 10000000000.0: # every time delta halves, remove points we are reasonably sure won't be SVs
+                X_range = numpy.where(self.a >= self.C/1000.)[0]
+                maxDelta = delta
+                if self.verbose:
+                    print ("Descent step magnitude:", delta)
+            elif maxDelta == 10000000000.0:
+                maxDelta = delta
+                if self.verbose:
+                    print ("Descent step magnitude:", delta)
 
         #get the data for support vectors
-        Qshrunk = Q[self.a >= self.C/100.,:][:,self.a >= self.C/100.]
+        Q.shrink(self.a >= self.C/100.)
         self.sv = X[self.a >= self.C/100., :]
         self.a = (self.a)[self.a >= self.C/100.]
 
         #calculate the contribution of all SVs
-        Qshrunk *= numpy.dot(self.a.reshape((-1,1)),self.a.reshape((1,-1)))
+        Q *= numpy.dot(self.a.reshape((-1,1)),self.a.reshape((1,-1)))
 
-        #this is needed for radius calculation apparently
-        self.bOffset = numpy.sum(Qshrunk.ravel())
+        #this is needed for radius calculation
+        self.bOffset = numpy.sum(Q.matrix().ravel())
+
         if self.verbose:
             print ("Number of support vectors:", len(self.a))
 
         """
         Select support vectors and solve for b to get the final classifier
         """
+        t0 = time.time()
         self.b = numpy.mean(self._predict_density(self.sv))
 
         if self.verbose:
-            print ("Bias value:", self.b)
+            print ("Bias value:", self.b, f"in {time.time()-t0}s")
 
         """
         Assign clusters to training dataset
@@ -168,50 +255,53 @@ class SimpleSVClustering:
         t0 = time.time()
         self._getAllClasses(self.sv)
         if self.verbose:
-            print(f"Clusters assigned in {t0-time.time()}s")
+            print(f"Clusters assigned in {time.time()-t0}s")
 
     def _predict_density(self, X):
         """
         For SVClustering, we need to calculate radius rather than bias.
         """
-        if (len(X.shape) < self.dims+1):
+        if (len(X.shape) == self.dims):
             X = X.reshape((1,-1))
-        clss = None
-        if X.shape[0] > self.dims:
-            clss = self.kernel(* ((X,X)), **self.kwargs)
-        else:
-            clss = numpy.array([self.kernel(* ((X,X)), **self.kwargs)])
-        for i in range(X.shape[0]):
-            clss[i] -= 2 * sum(self.a * self.kernel(* ((self.sv,numpy.repeat(X[i,:].reshape((1,-1)),len(self.sv),0))), **self.kwargs))
+        clss = self.kernel(X, X, **self.kwargs)
 
+        global _svc_pts
+        if _svc_pts is None:
+            num_processes = self.thread_pool._processes
+            self.thread_pool.close()
+            self.thread_pool = Pool(num_processes, initializer=_load_global_pts, initargs=(self.sv,))
+            _load_global_pts(self.sv)
+        results = _matrix_sumreduce_global(X, self.a, self.kernel, numpy.float16, self.thread_pool, self.kwargs)
+        clss -= 2 * results
         return (clss+self.bOffset)**0.5
-
-    def _predict(self, X):
-        """
-        Predict class for a single sample of data
-        """
-        for i in range(self.sv.shape[0]):
-            if self._checkClass(X,self.sv[i,:]):
-                return self.classifications[i]
-        return -1
 
     def predict(self, X):
         """
         Predict classes for out of sample data X
         """
-        if len(X.shape) > self.dims:
-            return numpy.array([self._predict(X[i,:]) for i in range(X.shape[0])])
-        return self._predict(X)
+        step_size = 1.0/(self.class_check_steps+1)
+        steps = numpy.arange(step_size,1.0-step_size,step_size, dtype=X.dtype)
+        if len(X.shape) == 1:
+            X = X.reshape((1,-1))
+        results = numpy.zeros(len(X))-1
+        for i in range(len(X)):
+            checkVals = self._checkClass(X[i], self.sv, steps)
+            in_cluster = numpy.where((checkVals <= self.b).ravel())[0]
+            if len(in_cluster):
+                #TODO: make this more resilient
+                results[i] = self.classifications[in_cluster[0]] #stats.mode(self.classifications[in_cluster])
+        return results
 
 
 if __name__ == '__main__':
-    import sklearn.datasets, time
-    data,labels = sklearn.datasets.make_moons(1000,noise=0.08,random_state=0)
+    import sklearn.datasets, time, matplotlib
+    data,labels = sklearn.datasets.make_moons(5000,noise=0.05,random_state=0)
     data -= numpy.mean(data,axis=0)
 
     #parameters can be sensitive, these ones work for two moons
     C = 0.1
-    clss = SimpleSVClustering(C,1e-10,rbfKernel,gamma=7.5)
+    gamma = numpy.array([12.5], dtype=numpy.float32)
+    clss = SimpleSVClustering(C,1e-8,rbfKernel,gamma=gamma)
     t0 = time.time()
     clss.fit(data)
     print(f"fit in {time.time()-t0} seconds")
@@ -219,13 +309,18 @@ if __name__ == '__main__':
     #check assigned classes for the two moons as a classification error
     t0 = time.time()
     t = clss.predict(data)
+    from ClusterQuality import KDB
     print(f"predicted in {time.time()-t0} seconds")
     print ("Error", numpy.sum((labels-t)**2) / float(len(data)))
+    t0 = time.time()
+    print ("KDB", KDB(data, t, clss.kernel, **clss.kwargs))
+    print(f"KDB calculated in {time.time()-t0}")
 
 
     from matplotlib import pyplot
 
     #generate a heatmap and display classified clusters.
+    matplotlib.use("Agg")
     a = numpy.zeros((100,100))
     for i in range(100):
         for j in range(100):
