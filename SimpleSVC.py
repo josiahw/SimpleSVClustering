016109b64efd2bfae4c11358726438c9aef547f6
Josiah Walker <merlz42@gmail.com>
2022-12-26 23:13:28 +0800

    major perf improvements, use sklearn kernel funcs

--- SimpleSVC.py
+++ SimpleSVC.py
@@ -5,18 +5,11 @@ Created on Mon Jul 11 20:40:56 2016
 @author: josiahw
 """
 import numpy, time, numpy.linalg
-from scipy.spatial.distance import cdist
-from scipy.optimize import linear_sum_assignment
-from scipy.interpolate import interp1d
-
-def polyKernel(a,b,pwr):
-    return numpy.dot(a,b.T)**pwr
-
-def rbfKernel(a,b,gamma):
-    """
-    1-dimensional rbf kernel
-    """
-    return numpy.exp(-gamma * numpy.linalg.norm(a - b, axis=-1))
+from sklearn.kernel_approximation import Nystroem
+from sklearn.metrics.pairwise import chi2_kernel, rbf_kernel, polynomial_kernel
+from MultiThreadedExecutor import MultiThreadedExecutor
+# we can have div by 0 in gradient descent
+numpy.seterr(divide='ignore')
 
 class SimpleSVClustering:
     w = None
@@ -31,6 +24,7 @@ class SimpleSVClustering:
     class_check_steps = 4
     incremental = False
     Qshrunk = None
+    nystroem = None
 
     def __init__(self,
                  C,
@@ -51,6 +45,17 @@ class SimpleSVClustering:
         self.tolerance = tolerance
         self.dtype = dtype
         self.kwargs = kwargs
+        self.k_neighbours = 60
+        self.threadDispatcher = MultiThreadedExecutor()
+
+        # set up interpolation step array (for checking connectedness)
+        step_size = 1.0/(self.class_check_steps+1)
+        steps = numpy.arange(step_size,1.0-step_size,step_size, dtype=self.dtype)
+        # order steps from the middle out, as that's the most likely spot to violate connectedness
+        middle_out = list(range(len(steps)))
+        middle_out.sort(key = lambda x: abs(len(steps) - x))
+        steps = steps[middle_out]
+        self.data_interpolation_steps = steps
 
     def _checkClass(self, a, b, steps):
         """
@@ -58,42 +63,69 @@ class SimpleSVClustering:
         It returns True if a and b are connected by a high probability region, false otherwise.
         NOTE: authors originally suggested 20 segments but that is SLOOOOOW, so we use 4. In practice it is pretty good.
         """
-        evals = numpy.array([self._predict_density(s * a + (1-s) * b) for s in steps])
-        return numpy.amax(evals, axis=0)
+        evals = None
+        #make sure b is the longer set of values
+        if len(a.shape) > 1:
+            c = b
+            b = a
+            a = c
+        if len(b.shape) > 1:
+            # record all values that violate connectedness assumptions, and only re-test those that don't yet violate
+            evals = self._predict_density(steps[0] * a + (1-steps[0]) * b)
+            candidates = numpy.unravel_index(numpy.flatnonzero(evals < self.b),evals.shape)[0]
+            for s in steps[1:]:
+                tests = self._predict_density(s * a + (1-s) * b[candidates])
+                evals[candidates] = tests
+                candidates = numpy.unravel_index(numpy.flatnonzero(evals < self.b),evals.shape)[0]
+                if len(candidates) == 0:
+                    break
+        else: 
+            # single-point case: return as soon as connectedness assumption is violated
+            for s in steps:
+                evals = [self._predict_density(s * a + (1-s) * b)]
+                if evals[0] >= self.b:
+                    break
+        return evals
 
-    def _getAllClasses(self, X):
+    def _getAllClasses(self, X, Q):
         """
         Assign class labels to each vector based on connected graph components.
-        TODO: The outputs of this should really be saved in order to embed new points into the clusters.
+        
         """
+        # Warning: we assume that only support vectors are fed into this function
 
-        #1: build the connected clusters
-        unvisited = numpy.array(list(range(len(X))))
+        # 1: build the connected clusters
+        # Visit unvisited points in order of largest alpha first, as it's likely to have direct contact with the most in-class points
+        # TODO: could be faster if refactored to use sets?.
+        unvisited = numpy.array(list(range(len(X))))[numpy.argsort(self.a)[::-1]]
         clusters = []
-        step_size = 1.0/(self.class_check_steps+1)
-        steps = numpy.arange(step_size,1.0-step_size,step_size, dtype=X.dtype)
+        steps = self.data_interpolation_steps
+        # test only k nearest neighbours - any further points will likely be connected to one of the knn's anyway
+        fit_qValue = numpy.quantile(Q.ravel(),1-min(self.k_neighbours, len(X))/len(X))
         while len(unvisited):
-            #create a new cluster with the first unvisited node
+            # create a new cluster with the first unvisited node
             c = [unvisited[0]]
             unvisited = unvisited[1:]
             i = 0
             t0 = time.time()
             while i < len(c) and len(unvisited):
-                #for all nodes in the cluster, add all connected unvisited nodes and remove them fromt he unvisited list
-                candidate_list = X[unvisited]
-                checkVals = self._checkClass(X[c[i]], candidate_list, steps)
-                in_cluster = unvisited[checkVals <= self.b]
+                # for all nodes in the cluster, add all connected unvisited nodes and remove them from the unvisited list
+                candidates = unvisited[Q[c[i], unvisited].ravel() > fit_qValue].ravel()
+                checkVals = self._checkClass(X[c[i]], X[candidates], steps)
+                in_cluster = candidates[checkVals <= self.b]
                 if len(in_cluster) > 0:
+                    noncandidates = unvisited[Q[c[i], unvisited].ravel() <= fit_qValue].astype(numpy.int32)
                     c.extend(in_cluster)
-                    unvisited = unvisited[checkVals > self.b]
+                    unvisited = numpy.concatenate([candidates[checkVals > self.b], noncandidates])
                 i += 1
             clusters.append(c)
+            
             if self.verbose:
                 print(f"Clustered {len(X)-len(unvisited)}/{len(X)} in {time.time()-t0}")
         # sort so largest clusters have the lowest index - allows us to easily remove outlier clusters later
         clusters.sort(key=len, reverse=True)
 
-        #3: group components by classification
+        # 3: group components by classification
         self.classifications = numpy.zeros(len(X))
         for i in range(len(clusters)):
             for c in clusters[i]:
@@ -101,25 +133,49 @@ class SimpleSVClustering:
         if self.verbose:
             print(f"Clusters: {len(numpy.unique(self.classifications))}")
             print(f"Cluster sizes: {[len(c) for c in clusters]}")
-        return self.classifications
 
     def fit_incremental(self, X, chunk_size = 5000):
         """
         fit data for SVM, using chunk_size points at a time and keeping previous support vectors
         """
+        # TODO: rewrite to be more efficient by merging chunked solutions (i.e. binary heap merges)
+        t0 = time.time()
         if X.shape[0] <= chunk_size:
+            self.incremental = False
             self.fit(X)
+            if self.verbose:
+                print(f"{len(self.sv)} support vectors fit in {time.time()-t0}s")
             return
         self.incremental = True
-        self.fit(X[:int(chunk_size)])
-        for i in range(1,int(numpy.ceil(X.shape[0] / chunk_size))):
-            if (i+1)*chunk_size < X.shape[0]:
-                self.fit(numpy.concatenate([self.sv, X[int(i*chunk_size):int((i+1)*chunk_size)]]))
-            else:
-                self.incremental = False
-                self.fit(numpy.concatenate([self.sv, X[int(i*chunk_size):X.shape[0]]]))
+        Q, sv = self.fit(X[:int(chunk_size)])
+        chunk_stack = [(1, Q, sv)]
+        if self.verbose:
+                print(f"{len(self.sv)} support vectors fit in {time.time()-t0}s")
+        i = 1
+        # merge chunks of equal depth in a binary merge strategy to ensure the least number of merges to finish the job
+        while i < int(numpy.ceil(X.shape[0] / chunk_size)) or len(chunk_stack) > 1:
+            # enqueue a new chunk of points
+            if len(chunk_stack) < 2 or chunk_stack[-1][0] != chunk_stack[-2][0] and i < int(numpy.ceil(X.shape[0] / chunk_size)):
+                start_index = int(i*chunk_size)
+                end_index = int(min((i+1)*chunk_size, X.shape[0]))
+                Q, sv = self.fit(numpy.concatenate([self.sv, X[start_index:end_index]]), Q)
+                chunk_stack.append((1, Q, sv))
+                i = i + 1
+            # merge existing chunks of points
+            elif len(chunk_stack) > 1:
+                chunk_depth, Q1, sv1 = chunk_stack.pop(-1)
+                _, Q2, sv2 = chunk_stack.pop(-1)
+                if self.verbose:
+                    print(f"merging", len(sv1)+len(sv2), "points at depth", chunk_depth)
+                # set incremental to false on the final merge
+                if i >= int(numpy.ceil(X.shape[0] / chunk_size)) and len(chunk_stack) == 0: self.incremental = False
+                Q, sv = self.fit(numpy.concatenate([sv1, sv2]), Q1, Q2)
+                del sv1, sv2, Q1, Q2
+                chunk_stack.append((chunk_depth + 1, Q, sv))
+            if self.verbose:
+                print(f"{len(self.sv)} support vectors fit in {time.time()-t0}s")
 
-    def fit(self, X):
+    def fit(self, X, Q1 = None, Q2 = None):
         """
         Fit to data X with labels y.
         """
@@ -127,14 +183,21 @@ class SimpleSVClustering:
         """
         Construct the Q matrix for solving
         """
-        Q = numpy.zeros((len(X),len(X)), dtype = self.dtype)
-        if self.incremental and self.Qshrunk is not None:
-            Q[:self.Qshrunk.shape[0],:self.Qshrunk.shape[1]] = self.Qshrunk
-        for i in range(len(X)):
-            start = i
-            if self.incremental and self.Qshrunk is not None:
-                start = self.Qshrunk.shape[1]
-            Q[i,start:] = self.kernel(numpy.tile(X[i], (X.shape[0]-start, 1)), X[start:], **self.kwargs) 
+        # TODO: implement nystrom approximation for Q to improve memory / compute
+        min_start = 0
+        max_end = len(X)
+        if Q1 is None:
+            Q = numpy.zeros((len(X),len(X)), dtype = self.dtype)
+        else:
+            Q = numpy.resize(Q1+1, (len(X),len(X)))
+            min_start = Q1.shape[1]
+            if not Q2 is None:
+                Q[min_start:,min_start:] = Q2
+                max_end = min_start
+            del Q1, Q2
+        for i in range(max_end):
+            start = max(i, min_start)
+            Q[start:, i] = Q[i, start:] = self.kernel(X[i].reshape(1, -1), X[start:], **self.kwargs)
 
         """
         Solve for a and w simultaneously by coordinate descent.
@@ -143,21 +206,25 @@ class SimpleSVClustering:
         """
         self.w = numpy.zeros(X.shape[1])
         self.a = numpy.zeros(X.shape[0])
+        min_a_val = self.C/100.
         delta = 10000000000.0
         maxDelta = delta
         # X_range keeps a record of values with non-zero alphas so we can reduce compute as we converge
         X_range = range(len(X))[:]
         while delta > self.tolerance:
             delta = 0.
+            g = None
             for i in X_range:
-                g = numpy.dot(Q[i], self.a) / Q[i,i] - 1.0
+                g = numpy.nan_to_num(numpy.divide(numpy.dot(Q[i], self.a), Q[i,i])) - 1.0
                 adelta = self.a[i] - min(max(self.a[i] - g, 0.0), self.C)
                 self.w += adelta * X[i]
                 delta += abs(adelta)
                 self.a[i] -= adelta
             delta /= len(X_range)
             if delta < maxDelta/2 and maxDelta < 10000000000.0: # every time delta halves, remove points we are reasonably sure won't be SVs
-                X_range = numpy.where(self.a >= self.C/1000.)[0]
+                # stackoverflow says flatnonzero is faster than numpy.where()
+                X_range = numpy.unravel_index(numpy.flatnonzero(self.a >= min_a_val),self.a.shape)[0]
+                self.a *= self.a >= min_a_val
                 maxDelta = delta
                 if self.verbose:
                     print ("Descent step magnitude:", delta)
@@ -166,19 +233,16 @@ class SimpleSVClustering:
                 if self.verbose:
                     print ("Descent step magnitude:", delta)
 
-        #get the data for support vectors
-        self.sv = X[self.a >= self.C/100., :]
-        Qshrunk = Q[self.a >= self.C/100.,:][:,self.a >= self.C/100.]
+        # get the data for support vectors
+        self.sv = X[self.a >= min_a_val, :]
+        Qshrunk = Q[self.a >= min_a_val,:][:,self.a >= min_a_val]
+        # stop early if we still ahve points to process
         if self.incremental:
-            self.Qshrunk = Qshrunk
-            return
-        self.a = (self.a)[self.a >= self.C/100.]
+            return Qshrunk, self.sv
+        self.a = (self.a)[self.a >= min_a_val]
 
-        #calculate the contribution of all SVs
-        Qshrunk *= numpy.dot(self.a.reshape((-1,1)),self.a.reshape((1,-1)))
-
-        #this is needed for radius calculation
-        self.bOffset = numpy.sum(Qshrunk.ravel())
+        # this is needed for radius calculation
+        self.bOffset = numpy.sum((Qshrunk * numpy.dot(self.a.reshape((-1,1)),self.a.reshape((1,-1)))).ravel()) 
         if self.verbose:
             print ("Number of support vectors:", len(self.a))
 
@@ -195,28 +259,27 @@ class SimpleSVClustering:
         Assign clusters to training dataset
         """
         t0 = time.time()
-        self._getAllClasses(self.sv)
+        self._getAllClasses(self.sv, Qshrunk)
         if self.verbose:
             print(f"Clusters assigned in {time.time()-t0}s")
+        
+        return Qshrunk, self.sv
 
     def _predict_density(self, X):
         """
         For SVClustering, we need to calculate radius rather than bias.
         """
-        if (len(X.shape) < self.dims+1):
-            X = X.reshape((1,-1))
-        clss = self.kernel(X, X, **self.kwargs)
-        for i in range(X.shape[0]):
-            clss[i] -= 2 * numpy.sum(self.a * self.kernel(X[i].reshape((1,-1)), self.sv, **self.kwargs))
+        # multithreading speeds up 
+        if len(X.shape) == 1:
+            return numpy.sqrt(numpy.subtract(self.kernel(X.reshape(1, -1), X.reshape(1, -1), **self.kwargs) + self.bOffset, 2 * numpy.dot(self.a, self.kernel(X.reshape(1, -1), self.sv, **self.kwargs).ravel())))
+        data_func = lambda x: numpy.subtract(self.kernel(x.reshape(1, -1), x.reshape(1, -1), **self.kwargs) + self.bOffset, 2 * numpy.dot(self.a, self.kernel(x.reshape(1, -1), self.sv, **self.kwargs).ravel()))
+        return numpy.sqrt(self.threadDispatcher.fill(data_func, X))
 
-        return (clss+self.bOffset)**0.5
-
-    def _predict(self, X):
+    def predict(self, X):
         """
-        Predict class for a single sample of data
+        Predict classes for out of sample data X
         """
-        step_size = 1.0/(self.class_check_steps+1)
-        steps = numpy.arange(step_size,1.0-step_size,step_size, dtype=X.dtype)
+        steps = self.data_interpolation_steps
         if len(X.shape) < 2:
             X = [X]
         classes = numpy.zeros(len(X), dtype=numpy.int64)-1
@@ -228,12 +291,6 @@ class SimpleSVClustering:
                     break
         return classes
 
-    def predict(self, X):
-        """
-        Predict classes for out of sample data X
-        """
-        return self._predict(X)
-
 
 if __name__ == '__main__':
     import sklearn.datasets, time, matplotlib
@@ -241,10 +298,10 @@ if __name__ == '__main__':
     data -= numpy.mean(data,axis=0)
 
     #parameters can be sensitive, these ones work for two moons
-    C = 0.1
-    clss = SimpleSVClustering(C,1e-9,numpy.float32,rbfKernel,gamma=10.5)
+    C = 0.05
+    clss = SimpleSVClustering(C,1e-7,numpy.float16,rbf_kernel,gamma=13)
     t0 = time.time()
-    clss.fit_incremental(data)
+    clss.fit_incremental(data,5000)
     print(f"fit in {time.time()-t0} seconds")
 
     #check assigned classes for the two moons as a classification error
